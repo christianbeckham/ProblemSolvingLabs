030ea4a5ca7b920c351de06bee3d2b91774cd1b3
Christian Beckham <urenacb22@gmail.com>
2022-08-19 14:00:58 -0500

    Refactored each task to use functions

--- labs/problem_solving_two.py
+++ labs/problem_solving_two.py
@@ -1,60 +1,92 @@
 # Tasks
 # Task 1: Reverse a String
-string = 'Hello'
-reversed_string = string[::-1]
-print('\nTask 1 result: ', reversed_string)
+def reverse_string(string):
+    new_string = ''
 
-# OR
-string = 'Hello'
-temp_character_array = []
+    for index in range(len(string) - 1, -1, -1):
+        new_string += string[index]
 
-for char in string:
-    temp_character_array.insert(0, char)
+    return new_string
 
-reversed_string_two = ''.join(temp_character_array)
-print('\nTask 1 result 2: ', reversed_string_two)
+
+string_to_reverse = 'Hello'
+result = reverse_string(string_to_reverse)
+print('\nTask 1 result: ', result)
 
 # Task 2: Capitalize a Letter
-string = 'hello world'
-temporary_string_array = []
-separator = ' '
 
-split_string = string.split(separator)
 
-for word in split_string:
-    temporary_string_array.append(word[0].capitalize() + word[1::])
+def capitalize_every_first_letter(phrase):
+    new_phrase = ''
 
-final_string = separator.join(temporary_string_array)
-print('\nTask 2 results: ', final_string)
+    for word in phrase.split(' '):
+        new_phrase += word.capitalize()
+        new_phrase += ' '
+
+    return new_phrase
+
+
+phrase = 'hello world'
+result = capitalize_every_first_letter(phrase)
+print('\nTask 2 result: ', result)
 
 # Task 3: Palindrome
-user_input = input("\nEnter a palindrome word to check: ")
-user_input_backwards = user_input[::-1]
 
-if user_input == user_input_backwards:
-    print(f'\tTask 3 results: The word {user_input} is a palindrome.')
-else:
-    print(f'\tTask 3 results: The word {user_input} is NOT a palindrome.')
+
+def reverse_string(string):
+    new_string = ''
+
+    for index in range(len(string) - 1, -1, -1):
+        new_string += string[index]
+
+    return new_string
+
+
+def remove_non_alphabetic_characters(string):
+    new_string = ''
+
+    for char in string:
+        if char.isalpha():
+            new_string += char
+
+    return new_string
+
+
+def check_is_palindrome(string):
+    string_characters_only = remove_non_alphabetic_characters(string)
+    reversed_string = reverse_string(string)
+    reversed_string_characters_only = remove_non_alphabetic_characters(
+        reversed_string)
+
+    if string_characters_only == reversed_string_characters_only:
+        return True
+    else:
+        return False
+
+
+user_input = input("\nEnter a palindrome word to check: ")
+result = check_is_palindrome(user_input)
+print(f'Task 3: Is {user_input} a palindrome... {result}')
 
 # Bonus Challenge
 # Task 4 : Compress a string of characters
-characters = 'aaabbbbbccccaacccbbbaaabbbaaa'
-compressed_characters = ''
-letter_count = 1
 
-for index in range(len(characters)):
-    current_letter = characters[index]
 
-    if index < (len(characters) - 1):
-        next_letter = characters[index + 1]
-    else:
-        next_letter = ''
+def compress_characters(string):
+    compressed_string = ''
+    count = 1
 
-    if next_letter == current_letter:
-        letter_count += 1
-    else:
-        output = str(letter_count) + current_letter
-        compressed_characters += output
-        letter_count = 1
+    for index in range(len(string)):
+        if index < len(string) - 1 and string[index] == string[index + 1]:
+            count += 1
+        else:
+            sequence = str(count) + string[index]
+            compressed_string += sequence
+            count = 1
 
-print('\nTask 4 results: ', compressed_characters)
+    return compressed_string
+
+
+characters_to_compress = 'aaabbbbbccccaacccbbbaaabbbaaa'
+result = compress_characters(characters_to_compress)
+print('\nTask 4 results: ', result)
